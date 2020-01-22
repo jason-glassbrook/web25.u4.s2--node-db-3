551b500a13f54c7412882d1bc9a2cf84fa3a2132
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-22 18:09:18 -0500

    consolidate requests with .route

--- routers/api/schemes/router.js
+++ routers/api/schemes/router.js
@@ -4,7 +4,15 @@ const { api : db } = require ('../../../models')
 
 const router = express.Router ()
 
-router.get ('/', (req, res) => {
+/***************************************
+  route /
+----------------------------------------
+  - GET -> db.schemas.getAll
+  - POST -> db.schemas.push
+***************************************/
+
+router.route ('/')
+.get ((req, res) => {
   db.schemes.getAll ()
   .then ((schemes) => {
     res.json (schemes)
@@ -15,8 +23,32 @@ router.get ('/', (req, res) => {
     .json ({ message : 'Failed to get schemes' })
   })
 })
+.post ((req, res) => {
+  const schemeData = req.body
 
-router.get ('/:scheme_id', (req, res) => {
+  db.schemes.push (schemeData)
+  .then ((scheme) => {
+    res
+    .status (201)
+    .json (scheme)
+  })
+  .catch ((error) => {
+    res
+    .status (500)
+    .json ({ message : 'Failed to create new scheme' })
+  })
+})
+
+/***************************************
+  route /:scheme_id
+----------------------------------------
+  - GET -> db.schemas.get
+  - PUT -> db.schemas.set
+  - DELETE -> db.schemas.pull
+***************************************/
+
+router.route ('/:scheme_id')
+.get ((req, res) => {
   const { scheme_id } = req.params
 
   db.schemes.get (scheme_id)
@@ -35,11 +67,72 @@ router.get ('/:scheme_id', (req, res) => {
     .json ({ message : 'Failed to get schemes' })
   })
 })
+.put ((req, res) => {
+  const { scheme_id } = req.params
+  const changes = req.body
 
-router.get ('/:scheme_id/steps', (req, res) => {
+  db.schemes.get (scheme_id)
+  .then ((scheme) => {
+    if (scheme) {
+      db.schemes.set (scheme_id, changes)
+      .then ((scheme) => {
+        res
+        .status (201)
+        .json (scheme)
+      })
+      .catch ((error) => {
+        res
+        .status (500)
+        .json ({ message : 'Failed to update scheme' })
+      })
+    }
+    else {
+      res
+      .status (404)
+      .json ({ message : 'Could not find scheme with given id' })
+    }
+  })
+  .catch ((error) => {
+    res
+    .status (500)
+    .json ({ message : 'Failed to update scheme' })
+  })
+})
+.delete ((req, res) => {
   const { scheme_id } = req.params
 
-  db.schemes.getOwnSteps (scheme_id)
+  db.schemes.pull (scheme_id)
+  .then ((scheme) => {
+    if (scheme) {
+      res
+      .status (200)
+      .json (scheme)
+    }
+    else {
+      res
+      .status (404)
+      .json ({ message : 'Could not find scheme with given id' })
+    }
+  })
+  .catch ((error) => {
+    res
+    .status (500)
+    .json ({ message : 'Failed to delete scheme' })
+  })
+})
+
+/***************************************
+  route /:scheme_id/steps
+----------------------------------------
+  - GET -> db.schemas.getOwnAllSteps
+  - POST -> db.schemas.pushOwnSteps
+***************************************/
+
+router.route ('/:scheme_id/steps')
+.get ((req, res) => {
+  const { scheme_id } = req.params
+
+  db.schemes.getOwnAllSteps (scheme_id)
   .then ((steps) => {
     if (steps.length) {
       res.json (steps)
@@ -55,24 +148,7 @@ router.get ('/:scheme_id/steps', (req, res) => {
     .json ({ message : 'Failed to get steps' })
   })
 })
-
-router.post ('/', (req, res) => {
-  const schemeData = req.body
-
-  db.schemes.push (schemeData)
-  .then ((scheme) => {
-    res
-    .status (201)
-    .json (scheme)
-  })
-  .catch ((error) => {
-    res
-    .status (500)
-    .json ({ message : 'Failed to create new scheme' })
-  })
-})
-
-router.post ('/:scheme_id/steps', (req, res) => {
+.post ((req, res) => {
   const stepData = req.body
   const { scheme_id } = req.params
 
@@ -104,59 +180,6 @@ router.post ('/:scheme_id/steps', (req, res) => {
   })
 })
 
-router.put ('/:scheme_id', (req, res) => {
-  const { scheme_id } = req.params
-  const changes = req.body
-
-  db.schemes.get (scheme_id)
-  .then ((scheme) => {
-    if (scheme) {
-      db.schemes.set (scheme_id, changes)
-      .then ((scheme) => {
-        res
-        .status (201)
-        .json (scheme)
-      })
-      .catch ((error) => {
-        res
-        .status (500)
-        .json ({ message : 'Failed to update scheme' })
-      })
-    }
-    else {
-      res
-      .status (404)
-      .json ({ message : 'Could not find scheme with given id' })
-    }
-  })
-  .catch ((error) => {
-    res
-    .status (500)
-    .json ({ message : 'Failed to update scheme' })
-  })
-})
-
-router.delete ('/:scheme_id', (req, res) => {
-  const { scheme_id } = req.params
-
-  db.schemes.pull (scheme_id)
-  .then ((scheme) => {
-    if (scheme) {
-      res
-      .status (200)
-      .json (scheme)
-    }
-    else {
-      res
-      .status (404)
-      .json ({ message : 'Could not find scheme with given id' })
-    }
-  })
-  .catch ((error) => {
-    res
-    .status (500)
-    .json ({ message : 'Failed to delete scheme' })
-  })
-})
+/**************************************/
 
 module.exports = router
