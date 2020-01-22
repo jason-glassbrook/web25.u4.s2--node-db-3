abf3392c6ffda7f0e09bbb9aaa7a2ee9f1fae6ed
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-22 17:31:39 -0500

    rewrite server using middleware, routers

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-const express = require ('express')
+/***********************************************************
+  server
+***********************************************************/
 
-const SchemeRouter = require ('./schemes/router.js')
+/// tools ///
+
+const express = require ('express')
+const helmet = require ('helmet')
+const morgan = require ('morgan')
+
+/***************************************
+  setup server
+***************************************/
 
 const server = express ()
 
-server.use (express.json ())
-server.use ('/api/schemes', SchemeRouter)
+/// wares ///
+
+server.use ([
+  helmet (),
+  morgan ('dev'),
+])
+
+/// routers ///
+
+server.use ('/api', require ('./routers/api').router)
+
+/// requests ///
+
+server.route ('*')
+.all ((ri, ro) => {
+  ro
+  .status (501)
+  .json ({
+    error : {
+      message : `not implemented : ${ri.method} ${ri.originalUrl}`,
+    }
+  })
+})
+
+/**************************************/
 
 module.exports = server
