f90290159335d6b1104b4189ff9a8e4a5a7264bf
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-22 17:28:27 -0500

    update code style

--- knexfile.js
+++ knexfile.js
@@ -2,21 +2,21 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true, // needed for sqlite
-    connection: {
-      filename: './data/schemes.db3',
+  development : {
+    client : 'sqlite3',
+    useNullAsDefault : true, // needed for sqlite
+    connection : {
+      filename : './data/schemes.db3',
     },
-    migrations: {
-      directory: './data/migrations'
+    migrations : {
+      directory : './data/migrations'
     },
-    seeds: {
-      directory: './data/seeds'
+    seeds : {
+      directory : './data/seeds'
     },
     // add the following
-    pool: {
-      afterCreate: (conn, done) => {
+    pool : {
+      afterCreate : (conn, done) => {
         // runs after a connection is made to the sqlite engine
         conn.run ('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
       },
